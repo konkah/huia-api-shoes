de0d89201ca6cd05b1dac7dddd45a8f06bddfea4
Karlos Helton Braga <karlos.hbraga@gmail.com>
2021-07-10 01:21:53 -0300

    Add Models and Main urls

--- huia_shoes/shoes_api/models.py
+++ huia_shoes/shoes_api/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Batch(models.Model):
+    identifier_code = models.CharField(max_length=100)
+    manufacturing_date = models.DateField()
+    product_qty = models.IntegerField()
+    
+    def __str__(self):
+        return self.identifier_code
+
+
+class Product(models.Model):
+    identifier_code = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    batch_number = models.ForeignKey(Batch, on_delete=models.PROTECT)
+    colours = [
+        ('red', 'Vermelho'),
+        ('blue', 'Azul'),
+        ('green', 'Verde'),
+        ('yellow', 'Amarelo'),
+        ('orange', 'Laranja'),
+        ('purple', 'Roxo'),
+
+    ]
+    colour = models.CharField(max_length=20, choices=colours, default='blue')
+    description = models.CharField(max_length=50)
+    value = models.FloatField()
+    
+    def __str__(self):
+        return self.identifier_code + ' - ' + self.name
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=11)
+    birth_date = models.DateField()
+    
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    order_number = models.IntegerField()
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+    seller = models.CharField(max_length=50)
+    products = models.ManyToManyField(Product)
+    total_value = models.FloatField()
+    
+    def __str__(self):
+        return self.order_number + ' - ' + self.total_value
